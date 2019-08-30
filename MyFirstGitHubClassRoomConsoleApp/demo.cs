3236ec6c7ca63ce2888f194bf18752eedc583616
Bartosz <DonMarchewa@gmail.com>
2019-08-30 13:32:26 +0200

    Some changes!

---
+++ MyFirstGitHubClassRoomConsoleApp/demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp
+{
+    class demo
+    {
+    int number { get; set; }
+    string stringus { get; set; }
+
+
+        public demo()
+        {
+
+        }
+
+    }
+}
